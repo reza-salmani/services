bcc440497c42ca1fc257ebf3223248c6dc874731
reza salmani <r.salmani.programming@gmail.com>
2024-11-26 13:42:39 +0330

    save

--- gate-way/src/app.controller.ts
+++ gate-way/src/app.controller.ts
@@ -19,3 +19,5 @@ export class AppController {
   }
 }
 //https://github.com/tkssharma/nestjs-kafka-monorepo/blob/develop/apps/api-gateway/src/app.controller.ts
+//https://github.com/israelio/nestjs-kafka-example/blob/master/src/app.controller.ts
+//https://blog.logrocket.com/microservices-nestjs-kafka-typescript/
